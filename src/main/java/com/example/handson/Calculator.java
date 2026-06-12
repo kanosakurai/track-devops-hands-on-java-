e890f3c043cc7396f055fb3e01d47d53bf485846
kanosakurai <kano.sakurai@tcs.com>
2026-06-12 14:10:17 +0900

    Update Calculator.java

--- src/main/java/com/example/handson/Calculator.java
+++ src/main/java/com/example/handson/Calculator.java
@@ -12,8 +12,7 @@ return a + b + c;
 
 private void validateRange(double value) { 
 if (value < 0 || value > 10) { 
-throw new IllegalArgumentException("value must be between 
-0 and 10"); 
+throw new IllegalArgumentException("value must be between 0 and 10"); 
 } 
 } 
 }
